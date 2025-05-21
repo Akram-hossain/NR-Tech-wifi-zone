af10f7b61ae5dc7ee755b3a49301708428b59b85
Akram Hossain <nayanbd17@gmail.com>
2025-05-21 19:18:25 +0600

    site updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // tabs
     const tabs = document.querySelectorAll('.tab');
     const contents = document.querySelectorAll('.tab-content');
 
@@ -15,15 +16,38 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    const btn = document.getElementById('backToTop');
 
     // Show button after scrolling down
+    const btn = document.getElementById('backToTop');
     window.addEventListener('scroll', () => {
         btn.classList.toggle('hidden', window.scrollY < 300);
     });
-
-    // Smooth scroll to top on click
     btn.addEventListener('click', () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
+
+
+    // top header fixed
+    const header = document.getElementById('mainHeader');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 40) {
+            header.classList.add('fixed', 'top-0', 'left-0', 'shadow-md', 'z-50', 'w-full', 'bg-white');
+        } else {
+            header.classList.remove('fixed', 'top-0', 'left-0', 'shadow-md', 'z-50', 'w-full', 'bg-white');
+        }
+    });
+
+    // active menu
+    const currentPath = window.location.pathname.replace(/\/$/, ''); // Remove trailing slash
+    const links = document.querySelectorAll('#mainMenu li a');
+
+    links.forEach(link => {
+        const linkPath = link.getAttribute('href').replace(/\/$/, '');
+        // alert(linkPath);
+        console.log({ linkPath, currentPath });
+        if (linkPath == currentPath || (linkPath == '' && currentPath == '')) {
+            link.classList.add('active'); // 🔴 Your active classes
+        }
+    });
+
 });
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   plugins: [injectHTML()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       index: './index.html',
-  //       contact: './contact.html', 
-  //       packages: './packages.html',
-  //       services: './services.html', 
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.html',
+        contact: './contact.html', 
+        packages: './packages.html',
+        services: './services.html', 
+      },
+    },
+  },
 });
